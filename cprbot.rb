e34c7bc0e0596744e78f903a8105fdf9a04f1adc
Vikram Dighe <vikram@swiftsignal.com>
2010-12-23 08:39:09 +0800

    Escaping HTML from the other Twitter methods.

--- cprbot.rb
+++ cprbot.rb
@@ -100,7 +100,7 @@ on :channel, /^:tw(eet|itter) @?(\w+)\^?(\d*) (\w+)\^?(\d*)/ do |_, user, offset
   begin
     info = twitter_client.statuses.user_timeline.json? :screen_name => user, :count => 1, :page => (offset || 1)
     info.first.try(:text).split("\n").each do |tweet_line|
-      msg channel, "#{requestor}: Tweet: #{user}: #{tweet_line}"
+      msg channel, "#{requestor}: Tweet: #{user}: #{CGI.unescapeHTML(tweet_line)}"
     end
   rescue
     msg channel, "Fail whale. Sorry."
@@ -111,7 +111,7 @@ on :channel, /^:tw(eet|itter) @?(\w+)\^?(\d*)/ do |_, user, offset|
   begin
     info = twitter_client.statuses.user_timeline.json? :screen_name => user, :count => 1, :page => (offset || 1)
     info.first.try(:text).split("\n").each do |tweet_line|
-      msg channel, "Tweet: #{user}: #{tweet_line}"
+      msg channel, "Tweet: #{user}: #{CGI.unescapeHTML(tweet_line)}"
     end
   rescue
     msg channel, "Fail whale. Sorry."
