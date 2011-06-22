922171debc1dbb09b94a632ad2e9f5d4449aa64f
Joshua Miller <elefantstn@gmail.com>
2011-06-22 10:14:42 -0400

    Need to include RTs in both places.

--- cprbot.rb
+++ cprbot.rb
@@ -130,7 +130,7 @@ end
 
 on :channel, /^:tw(eet|itter) @?(\w+)\^?(\d*)/ do |_, user, offset|
   begin
-    info = twitter_client.statuses.user_timeline.json? :screen_name => user, :count => 1, :page => (offset || 1)
+    info = twitter_client.statuses.user_timeline.json? :screen_name => user, :count => 1, :page => (offset || 1), :include_rts => true
     info.first.try(:text).split("\n").each do |tweet_line|
       msg channel, "Tweet: #{user}: #{CGI.unescapeHTML(tweet_line)}"
     end
