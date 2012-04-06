bad2601c50960194f919d8d4e29d799e44f0860f
Vikram Dighe <vikram@swiftsignal.com>
2012-04-05 20:02:20 -0400

    Fixed a syntax error.

--- cprbot.rb
+++ cprbot.rb
@@ -271,7 +271,7 @@ on :channel, /^:boobs/ do
   msg channel, boobs
 end
 
-on :channel /^:(fight|fite) (\w+) (\w+)/ do |nick1,nick2|
+on :channel, /^:(fight|fite) (\w+) (\w+)/ do |nick1,nick2|
 	msg channel, "LET'S GET IT ON"
 	rand1 = (Random.rand(20) + 1).to_s
 	rand2 = (Random.rand(20) + 1).to_s
